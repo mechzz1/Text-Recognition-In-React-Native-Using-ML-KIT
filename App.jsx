bd7b2e4847d3d2dda0aab8f00e0626afb8a0aa1c
Munib <munib.siddiqui110@gmail.com>
2024-10-10 17:05:54 +0500

    project init

--- App.jsx
+++ App.jsx
@@ -1,109 +1,249 @@
-
-
+import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import {
-  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Image,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
-
-import { launchImageLibrary, launchCamera } from 'react-native-image-picker'
+import { launchCamera } from 'react-native-image-picker';
 import TextRecognition from '@react-native-ml-kit/text-recognition';
 
+const Stack = createStackNavigator();
 
+function SplashScreen({ navigation }) {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('Home');
+    }, 3000);
+  }, []);
 
+  return (
+    <View style={styles.splashContainer}>
+      <Image
+        source={require('./assets/logo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      {/* <Text style={styles.splashText}>My App</Text> */}
+    </View>
+  );
+}
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [image, setImage] = useState("")
-  const [text, setText] = useState()
+function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={() => navigation.navigate('ScanID')}
+        >
+          <Text style={styles.buttonText}>Scan ID Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={() => navigation.navigate('AddVisitor')}
+        >
+          <Text style={styles.buttonText}>Add Visitor</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
 
-
-
-  const pickImage = async () => {
-    console.log("working");
-    // No permissions request is necessary for launching the image library
-    let result = await launchImageLibrary({ mediaType: 'photo' })
-    if (result != undefined) {
-      setImage(result.assets[0].uri)
-    }
-  };
+function ScanIDScreen() {
+  const [image, setImage] = useState("");
+  const [text, setText] = useState({
+    Name: "",
+    'Identity Number': ""
+  });
 
   const openCamera = async () => {
     let result = launchCamera({
       mediaType: 'photo'
     }, (result) => {
-      setImage(result.assets[0].uri)
+      if (result.assets && result.assets.length > 0) {
+        setImage(result.assets[0].uri);
+      }
     });
   }
-
-
-
-
 
   const recognizeText = async () => {
     if (image != "") {
       const result = await TextRecognition.recognize(image);
       if (result != undefined) {
-        setText(result.text)
+        let idCardRegex = /\b\d{5}-\d{7}-\d{1}\b/;
+        const output = result.text;
+        let matchResult = output.match(idCardRegex);
+
+        matchResult = matchResult ? matchResult[0] : "Not Clear Image";
+        let parsedData = parseOCRText(result.text);
+        parsedData['Identity Number'] = matchResult;
+        setText(parsedData);
+        console.log(parsedData);
+      }
+    }
+  }
+
+  function parseOCRText(text) {
+    const lines = text.split('\n');
+    const parsedData = {};
+    const keyMap = {
+      "Name": "Name", 
+    };
+
+    let currentKey = 0;
+
+    for (const line of lines) {
+      if (keyMap[line]) {
+        currentKey = keyMap[line];
+      } else if (currentKey) {
+        parsedData[currentKey] = line;
+        currentKey = null;
       }
     }
 
+    return parsedData;
   }
 
   useEffect(() => {
-    recognizeText()
-  }, [image])
-
-
+    recognizeText();
+  }, [image]);
 
   return (
-    <SafeAreaView>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-      >
-
-
-        <View style={{ justifyContent: 'center', alignItems: "center", marginTop: 64 }}>
-          <Text>Testing</Text>
-          <View style={{ flexDirection: "row" }}>
-            <Button onPress={pickImage} title='Pick Image' />
-            <Button onPress={openCamera} title='open Camera' />
-          </View>
-          <Text style={{ textAlign: 'justify', fontSize: 32 }}>{text}</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.contentContainer}>
+          <TouchableOpacity style={styles.button} onPress={openCamera}>
+            <Text style={styles.buttonText}>Open Camera</Text>
+          </TouchableOpacity>
+          <Text style={styles.resultText}>{text.Name}</Text>
+          <Text style={styles.resultText}>{text['Identity Number']}</Text>
         </View>
       </ScrollView>
-    </SafeAreaView >
+    </SafeAreaView>
+  );
+}
+
+function AddVisitorScreen() {
+  const [name, setName] = useState('');
+  const [idNumber, setIdNumber] = useState('');
+
+  const handleSubmit = () => {
+    console.log('Submitted:', { name, idNumber });
+    // Here you would typically save this data or send it to an API
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.contentContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={setName}
+            value={name}
+            placeholder="Visitor Name"
+            placeholderTextColor="#800073"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={setIdNumber}
+            value={idNumber}
+            placeholder="ID Card Number"
+            keyboardType="numeric"
+            placeholderTextColor="#800073"
+
+          />
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="ScanID" component={ScanIDScreen} />
+        <Stack.Screen name="AddVisitor" component={AddVisitorScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  splashContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
   },
-  sectionDescription: {
-    marginTop: 8,
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  splashText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#800073',
+  },
+  scrollView: {
+    flexGrow: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#800073',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+    marginBottom: 20,
+    width: '80%',
+  },
+  buttonText: {
+    color: 'white',
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  resultText: {
+    fontSize: 24,
+    color: '#800073',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: '#800073',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    color: '#800073',
+
   },
 });
 
